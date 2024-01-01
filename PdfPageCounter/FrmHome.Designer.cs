55481ace6eb3947c22e2b5b530022fa90c815c96
pareshpbhayani <pareshpbhayani@gmail.com>
2024-01-01 10:26:18 +0530

    Add project files.

---
+++ PdfPageCounter/FrmHome.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace PdfPageCounter
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHome));
+            btnBrowse = new Button();
+            txtFolderPath = new TextBox();
+            txtTotalCount = new TextBox();
+            progressBar1 = new ProgressBar();
+            lblProcessStatus = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Location = new Point(213, 45);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(75, 37);
+            btnBrowse.TabIndex = 0;
+            btnBrowse.Text = "Browse";
+            btnBrowse.UseVisualStyleBackColor = true;
+            btnBrowse.Click += btnBrowse_Click;
+            // 
+            // txtFolderPath
+            // 
+            txtFolderPath.Enabled = false;
+            txtFolderPath.Location = new Point(12, 14);
+            txtFolderPath.Name = "txtFolderPath";
+            txtFolderPath.Size = new Size(476, 25);
+            txtFolderPath.TabIndex = 1;
+            // 
+            // txtTotalCount
+            // 
+            txtTotalCount.Enabled = false;
+            txtTotalCount.Location = new Point(149, 88);
+            txtTotalCount.Name = "txtTotalCount";
+            txtTotalCount.Size = new Size(203, 25);
+            txtTotalCount.TabIndex = 1;
+            txtTotalCount.TextAlign = HorizontalAlignment.Center;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 121);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(476, 23);
+            progressBar1.TabIndex = 2;
+            // 
+            // lblProcessStatus
+            // 
+            lblProcessStatus.AutoSize = true;
+            lblProcessStatus.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lblProcessStatus.ForeColor = Color.Green;
+            lblProcessStatus.Location = new Point(180, 148);
+            lblProcessStatus.Name = "lblProcessStatus";
+            lblProcessStatus.Size = new Size(141, 19);
+            lblProcessStatus.TabIndex = 3;
+            lblProcessStatus.Text = "Process completed!";
+            lblProcessStatus.Visible = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Maroon;
+            label1.Location = new Point(358, 91);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 19);
+            label1.TabIndex = 3;
+            label1.Text = "Counts ony PDFs.";
+            label1.Visible = false;
+            // 
+            // FrmHome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(500, 171);
+            Controls.Add(label1);
+            Controls.Add(lblProcessStatus);
+            Controls.Add(progressBar1);
+            Controls.Add(txtTotalCount);
+            Controls.Add(txtFolderPath);
+            Controls.Add(btnBrowse);
+            Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FrmHome";
+            Text = "PDF Page Counter";
+            Load += FrmHome_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnBrowse;
+        private TextBox txtFolderPath;
+        private TextBox txtTotalCount;
+        private ProgressBar progressBar1;
+        private Label lblProcessStatus;
+        private Label label1;
+    }
+}
---
+++ PdfPageCounter/FrmHome.cs
@@ -0,0 +1,161 @@
+using iTextSharp.text.pdf;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace PdfPageCounter
+{
+    public partial class FrmHome : Form
+    {
+        //private delegate void ProgressHandler(string);
+
+        class UnsortedFilesInfo
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+        }
+
+        class SortedFilesInfo
+        {
+            public int FileName { get; set; }
+            public string FullFileName { get; set; }
+            public string FilePath { get; set; }
+        }
+
+        public FrmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmHome_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var dailog = new FolderBrowserDialog())
+            {
+                dailog.ShowDialog();
+                txtFolderPath.Text = dailog.SelectedPath;
+                lblProcessStatus.Visible = false;
+                txtTotalCount.Text = "Wait...";
+                progressBar1.Value = 0;
+            }
+            Thread pageCounterThread = new Thread(() => CountFileAndPages(txtFolderPath.Text));
+            pageCounterThread.Start();
+        }
+
+        private void CountFileAndPages(string folderPath)
+        {
+            if (!string.IsNullOrEmpty(folderPath))
+            {
+                int pageCounter = 0;
+                var files = Directory.GetFiles(folderPath, "*.pdf", SearchOption.AllDirectories);
+                int fileCounter = files.Length;
+                int processCounter = 0;
+
+                List<int> sortedFiles = new();
+                //List<string> unsortedFiles = new();
+
+                //ScanFilesInfo scanFilesInfo = n
+
+                List<SortedFilesInfo> sortedFilesList = new();
+                List<UnsortedFilesInfo> unsortedFilesList = new();
+
+                foreach (var item in files)
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(item);
+                    var splitedFileNameArray = fileName.Split(new char[] { '-', '_', '(' });
+                    string splitedFileName;
+                    if (splitedFileNameArray.Length > 1)
+                    {
+                        splitedFileName = splitedFileNameArray[0];
+                    }
+                    else
+                    {
+                        splitedFileName = fileName;
+                    }
+
+                    if (int.TryParse(splitedFileName, out int intFileName))
+                    {
+                        sortedFilesList.Add(new SortedFilesInfo { FileName = intFileName, FullFileName = fileName, FilePath = item });
+                        //sortedFiles.Add(intFileName);
+                    }
+                    else
+                    {
+                        unsortedFilesList.Add(new UnsortedFilesInfo { FileName = fileName, FilePath = item });
+                        //unsortedFiles.Add(splitedFileName);
+                    }
+                }
+                //sortedFiles.Sort();
+
+                sortedFilesList = sortedFilesList.OrderBy(o => o.FileName).ToList();
+
+                string filePath = "Scan Report.txt";
+
+                File.Create(filePath).Close();
+
+                using (StreamWriter stramWriter = File.AppendText(filePath))
+                {
+                    int totalSortedFiles = 0;
+                    int totalSortedPages = 0;
+                    int totalUnsortedFiles = 0;
+                    int totalUnsortedPages = 0;
+                    stramWriter.WriteLine("-------------- Files with number --------------");
+                    //Process sorted files
+                    foreach (var item in sortedFilesList)
+                    {
+                        processCounter++;
+                        using (PdfReader pdfReader = new PdfReader(item.FilePath))
+                        {
+                            pageCounter += pdfReader.NumberOfPages;
+                            stramWriter.WriteLine(string.Format("{0, -4}", processCounter) + string.Format("{0, -60}", item.FullFileName) + "\t>\t" + pdfReader.NumberOfPages);
+                        }
+
+                        progressBar1.BeginInvoke(() =>
+                        {
+                            progressBar1.Value = (processCounter * 100 / fileCounter);
+                        });
+                    }
+                    totalSortedFiles = processCounter;
+                    totalSortedPages = pageCounter;
+
+                    stramWriter.WriteLine("-------------- Unknown or Files with name --------------");
+                    //Process unsorted files
+                    foreach (var item in unsortedFilesList)
+                    {
+                        processCounter++;
+                        using (PdfReader pdfReader = new PdfReader(item.FilePath))
+                        {
+                            pageCounter += pdfReader.NumberOfPages;
+                            stramWriter.WriteLine(string.Format("{0, -4}", processCounter) + string.Format("{0, -60}", item.FileName) + "\t>\t" + pdfReader.NumberOfPages);
+                        }
+
+                        progressBar1.BeginInvoke(() =>
+                        {
+                            progressBar1.Value = (processCounter * 100 / fileCounter);
+                        });
+                    }
+                    totalUnsortedFiles = processCounter - totalSortedFiles;
+                    totalUnsortedPages = pageCounter - totalSortedPages;
+                    stramWriter.WriteLine("-------------- -------------- --------------");
+                    stramWriter.WriteLine("Total Number Files: " + totalSortedFiles + " | Total Number Pages: " + totalSortedPages);
+                    stramWriter.WriteLine("Total Unknown : " + totalUnsortedFiles + " | Total Unknown Pages: " + totalUnsortedPages);
+                    stramWriter.WriteLine("Total Files: " + fileCounter + " | Total Pages: " + pageCounter);
+                    stramWriter.Close();
+                }
+
+                txtTotalCount.BeginInvoke(() =>
+                {
+                    txtTotalCount.Text = "Files: " + fileCounter + " | Pages: " + pageCounter;
+                });
+
+                lblProcessStatus.BeginInvoke(() =>
+                {
+                    lblProcessStatus.Visible = true;
+                });
+
+                Process.Start("notepad.exe",filePath);
+            }
+        }
+    }
+}
